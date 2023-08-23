525cce079d8e99d34c0015f6075b6c66e96f33ff
wisdomin121@naver.com <wisdomin121@naver.com>
2023-08-24 01:12:52 +0900

    feat: 라우트 설정
    Co-authored-by: SengrokYoon <sonicrok93@gmail.com>

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,3 +1,16 @@
 import axios from 'axios'
 
-export const axiosInstance = axios.create({})
+const BASE_URL = 'https://www.pre-onboarding-selection-task.shop/'
+
+export const NoAuthInstance = axios.create({ 
+  baseURL: BASE_URL, 
+  headers: { "Content-Type": "application/json" } 
+})
+
+export const AuthInstance = axios.create({
+  baseURL: BASE_URL, 
+  headers: { 
+    "Content-Type": "application/json", 
+    Authorization: `Bearer ${localStorage.getItem('access_token')}`
+  }
+})
---
+++ src/pages/PageRouter.tsx
@@ -0,0 +1,34 @@
+import { Navigate, Route, BrowserRouter, Routes } from 'react-router-dom'
+
+import Home from './Home'
+import Todo from './Todo'
+import Signin from './Signin'
+import Signup from './Signup'
+
+interface IPublicRoute {
+  isSignIn: boolean
+}
+
+function PageRouter() {
+  const isAuth = localStorage.getItem('access_token')
+
+  function ProtectedRoute() {
+    return isAuth ? <Todo /> : <Navigate to="/signin" />
+  }
+  function PublicRoute({ isSignIn }: IPublicRoute) {
+    return isAuth ? <Navigate to="/todo" /> : isSignIn ? <Signin /> : <Signup />
+  }
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/todo" element={<ProtectedRoute />} />
+        <Route path="/signin" element={<PublicRoute isSignIn={true} />} />
+        <Route path="/signup" element={<PublicRoute isSignIn={false} />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default PageRouter
