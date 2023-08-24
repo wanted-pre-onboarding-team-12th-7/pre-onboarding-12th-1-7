368a20bdee3f60c4b2ace2451eebb72cf4c5f1b7
anyl92 <yulim9592@gmail.com>
2023-08-24 12:21:27 +0900

    feat: Sign styled-component 작성
    Co-authored-by: JangHyunjeong <jang.hyunjeong01@gmail.com>

---
+++ src/components/common/Sign/SignForm.styled.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const StyledSignFormContainer = styled.div`
+  width: 100%;
+`
+
+export const StyledSignFormTitle = styled.div`
+  font-size: 30px;
+  text-align: center;
+`
+
+export const StyledSignFormBox = styled.div`
+  width: 50%;
+  margin: 20px auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
