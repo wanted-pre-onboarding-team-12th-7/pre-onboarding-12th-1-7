a5d8ea5c7e502b403c97fc46682673b3ac25e678
SalmonTaker <seungwon.dev@gmail.com>
2023-08-24 13:59:18 +0900

    feat: todo api 작성
    Co-Authored-By: 이선근 <tjsrms1227@gmail.com>

---
+++ src/apis/todo.ts
@@ -0,0 +1,25 @@
+import { AuthInstance } from './instance'
+import { TodoType } from '../components/TodoList/types'
+
+export const createTodoRequest = async (todo: string): Promise<TodoType> => {
+  const { data } = await AuthInstance.post('/todos', { todo })
+  return data
+}
+
+export const getTodosRequest = async (): Promise<TodoType[]> => {
+  const { data } = await AuthInstance.get('/todos')
+  return data
+}
+
+export const updateTodoRequest = async (
+  id: number,
+  todo: string,
+  isCompleted: boolean
+): Promise<TodoType> => {
+  const { data } = await AuthInstance.put(`/todos/${id}`, { todo, isCompleted })
+  return data
+}
+
+export const deleteTodoRequest = (id: number) => {
+  return AuthInstance.delete(`/todos/${id}`)
+}
