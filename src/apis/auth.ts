d61d04bc8207247572cd1735b29cd8f024397fb9
anyl92 <yulim9592@gmail.com>
2023-08-24 11:07:32 +0900

    feat: async 추가 Co-authored-by: JangHyunjeong <jang.hyunjeong01@gmail.com>

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,17 +1,17 @@
 import { NoAuthInstance } from "./instance"
 
-export const postSignUp = (email: string, password: string) => {
+export const postSignUp = async (email: string, password: string) => {
   try {
-    NoAuthInstance.post('/auth/signup', {email, password});
+    await NoAuthInstance.post('/auth/signup', {email, password});
   } catch (err) {
     console.error(err);
     throw err;
   }
 }
 
-export const postSignin = (email: string, password: string) => {
+export const postSignin = async (email: string, password: string) => {
   try {
-    NoAuthInstance.post(`/auth/signin`, {email, password});
+    await NoAuthInstance.post(`/auth/signin`, {email, password});
   } catch (err) {
     console.error(err);
     throw err;
