af58ded6b44ff9eb8c4916c0a9a1b02fd27610aa
SalmonTaker <seungwon.dev@gmail.com>
2023-08-24 14:00:32 +0900

    feat: todo 페이지 작성
    Co-Authored-By: 이선근 <tjsrms1227@gmail.com>

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom'
+import TodoList from '../components/TodoList/TodoList'
 
 function Todo() {
   const navigate = useNavigate()
@@ -6,6 +7,7 @@ function Todo() {
   return (
     <div>
       투두리스트 페이지입니다.
+      <TodoList />
       <button
         onClick={() => {
           localStorage.removeItem('access_token')
