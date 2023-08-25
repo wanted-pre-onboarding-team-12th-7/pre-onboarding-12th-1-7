631e257d782161db97cd91183d963f07914c3fb1
Seungrok <sonicrok93@gmail.com>
2023-08-25 19:59:57 +0900

    refactor: ContextAPI를 활용하여 SignForm 리팩토링

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,9 +1,17 @@
 import SignForm from '../components/common/Sign/SignForm'
+import { StyledSignFormContainer } from '../components/common/Sign/SignForm.styled'
 
 function SignupPage() {
   return (
     <div>
-      <SignForm isSignUp={true} />
+      <StyledSignFormContainer>
+        <SignForm.Title title="회원가입" />
+        <SignForm.Form mode="signup">
+          <SignForm.Id />
+          <SignForm.Password />
+          <SignForm.ButtonGroup mode="signup" />
+        </SignForm.Form>
+      </StyledSignFormContainer>
     </div>
   )
 }
